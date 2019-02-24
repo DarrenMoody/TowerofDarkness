9c24338aa9e2dc1952f327f649b167eaad399730
Darren Moody <darren@paladinarcher.com>
2019-02-23 18:03:09 -0700

    Initial commit

---
+++ app/src/main/java/com/moodytoons/gaming/towerofdarkness/ActivityMain.java
@@ -0,0 +1,14 @@
+package com.moodytoons.gaming.towerofdarkness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
